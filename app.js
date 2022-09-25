6a3a45a5bc9c78bd524b5177e7905afb40e2f047
taddesbt <taddesbt@gmail.com>
2022-09-25 14:44:54 -0400

    add start files

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const message = document.querySelector(".message");
+const guess = document.querySelector("input");
+const button = document.querySelector("button");
+let inplay = false;
+let scramble = "";
+let scrambled = "";
+let score = 0;
+const myArray = ["javascript", "website", "html", "document", "course", "new"];
+
